18f5ebc0cbb9c5ddec30c73b65c1f7c43cc63de9
Brendan Ward <bcward@consbio.org>
2015-11-09 21:19:16 -0800

    Fixed setup.py so it now installs things properly

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='simple-lpk',
-    version='0.0.1',
-    packages=[],
+    version='0.0.2',
     url='https://github.com/databasin/simple-lpk',
     license='see LICENSE',
     author='databasin',
     author_email='databasinadmin@consbio.org',
     description='Simple tool for creating an ArcGIS layer package from a shapefile',
+    py_modules=['pkg'],
+    data_files=[('lyr', ['lyr/line.lyr', 'lyr/point.lyr', 'lyr/polygon.lyr'])],
     entry_points='''
         [console_scripts]
         pkg_lpk=pkg:pkg_lpk
